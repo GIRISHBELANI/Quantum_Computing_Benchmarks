a0ba1686d9b6f3b2c87872c5e9ddc93e7118f468
GIRISHBELANI <girish.belani05@gmail.com>
2024-07-11 12:12:55 +0530

    for Cirq, implemented functionality for calculating Algorithmic Depth and Normalized/Transpiled Depth, which results in plotting Volumetric Positioning Plot

--- qc-app-oriented-benchmarks/_common/cirq/execute.py
+++ qc-app-oriented-benchmarks/_common/cirq/execute.py
@@ -27,11 +27,22 @@ import time
 import copy
 import metrics
 
+import logging
+# logger for this module
+logger = logging.getLogger(__name__)
+
 import cirq
 backend = cirq.Simulator()      # Use Cirq Simulator by default
 
-#noise = 'DEFAULT'
-noise=None
+# Option to compute normalized depth during execution (can disable to reduce overhead in large circuits)
+use_normalized_depth = True
+
+# Option to perform explicit transpile to collect depth metrics
+# (disabled after first circuit in iterative algorithms)
+do_transpile_metrics = True
+
+# noise = 'DEFAULT'
+noise = None
 
 # Initialize circuit execution module
 # Create array of batched circuits and a dict of active circuits 
@@ -43,6 +54,12 @@ result_handler = None
 
 device=None
 
+# Print progress of execution
+verbose = False
+
+# Print additional time metrics for each stage of execution
+verbose_time = False
+
 # Special object class to hold job information and used as a dict key
 class Job:
     pass
@@ -53,6 +70,9 @@ def init_execution (handler):
     batched_circuits.clear()
     active_circuits.clear()
     result_handler = handler
+        
+    # On initialize, always set trnaspilation for metrics and execute to True
+    set_tranpilation_flags(do_transpile_metrics=True, do_transpile_for_execute=True)
     
     # create an informative device name
     # this should be move to set_execution_target method later
@@ -96,7 +116,9 @@ def set_noise_model(noise_model = None):
     # see reference on NoiseModel here https://quantumai.google/cirq/noise
     global noise
     noise = noise_model
-
+    
+# set_noise_model(noise_model = "DEFAULT")   # to execute with DEFAULT noise
+set_noise_model(noise_model = None)   # for noise-free model
 
 # Submit circuit for execution
 # This version executes immediately and calls the result handler
@@ -106,8 +128,9 @@ def submit_circuit (qc, group_id, circuit_id, shots=100):
     batched_circuits.append(
         { "qc": qc, "group": str(group_id), "circuit": str(circuit_id),
             "submit_time": time.time(), "shots": shots }
-    );
+    )
     #print("... submit circuit - ", str(batched_circuits[len(batched_circuits)-1]))
+
     
     
 # Launch execution of all batched circuits
@@ -127,6 +150,31 @@ def execute_circuit (batched_circuit):
     # Initiate execution 
     job = Job()
     circuit = batched_circuit["qc"]
+
+    # obtain initial circuit metrics
+    qc_depth, qc_size, qc_count_ops, qc_xi, qc_n2q = get_circuit_metrics(circuit)
+
+    # default the normalized transpiled metrics to the same, in case exec fails
+    qc_tr_depth = qc_depth
+    qc_tr_size = qc_size
+    qc_tr_count_ops = qc_count_ops
+    qc_tr_xi = qc_xi; 
+    qc_tr_n2q = qc_n2q
+    #print(f"... before tp: {qc_depth} {qc_size} {qc_count_ops}")
+
+    try:    
+        # transpile the circuit to obtain size metrics using normalized basis
+        if do_transpile_metrics and use_normalized_depth:
+            qc_tr_depth, qc_tr_size, qc_tr_count_ops, qc_tr_xi, qc_tr_n2q = transpile_for_metrics(circuit)
+            
+            # we want to ignore elapsed time contribution of transpile for metrics (normalized depth)
+            active_circuit["launch_time"] = time.time()
+
+    except Exception as e:
+        print(f'ERROR: Failed to execute circuit {active_circuit["group"]} {active_circuit["circuit"]}')
+        print(f"... exception = {e}")
+        return
+
     if type(noise) == str and noise == "DEFAULT":
         # depolarizing noise on all qubits
         circuit = circuit.with_noise(cirq.depolarize(0.05))
@@ -141,6 +189,17 @@ def execute_circuit (batched_circuit):
         device.validate_circuit(circuit)
 
     job.result = backend.run(circuit, repetitions=shots)
+
+    # store circuit dimensional metrics
+    metrics.store_metric(active_circuit["group"], active_circuit["circuit"], 'depth', qc_depth)
+    metrics.store_metric(active_circuit["group"], active_circuit["circuit"], 'size', qc_size)
+    metrics.store_metric(active_circuit["group"], active_circuit["circuit"], 'xi', qc_xi)
+    metrics.store_metric(active_circuit["group"], active_circuit["circuit"], 'n2q', qc_n2q)
+
+    metrics.store_metric(active_circuit["group"], active_circuit["circuit"], 'tr_depth', qc_tr_depth)
+    metrics.store_metric(active_circuit["group"], active_circuit["circuit"], 'tr_size', qc_tr_size)
+    metrics.store_metric(active_circuit["group"], active_circuit["circuit"], 'tr_xi', qc_tr_xi)
+    metrics.store_metric(active_circuit["group"], active_circuit["circuit"], 'tr_n2q', qc_tr_n2q)
     
     # put job into the active circuits with circuit info
     active_circuits[job] = active_circuit
@@ -196,4 +255,179 @@ def wait_for_completion():
 # Test circuit execution
 def test_execution():
     pass
+
+#########################################################################################################
+# Get circuit metrics fom the circuit passed in
+def get_circuit_metrics(qc):
+
+    logger.info('Entering get_circuit_metrics')
+    #print(qc)
     
+    # obtain initial circuit size metrics
+    qc_depth = len(cirq.Circuit(qc.all_operations()))
+    qc_size = count_gate_operations(qc)
+    qc_count_ops = count_ops(qc)
+
+    # print("Algorithmic Depth ===== ", qc_depth)
+    # print("Total no. of gate operations (including inside sub-circuit) ===== ", qc_size)
+    # print("qc_count_ops ===== ", qc_count_ops)
+
+    # Print the operation counts
+    # print("Operation counts in the circuit:")
+    # for gate, count in qc_count_ops.items():
+    #     print(f"{gate}: {count}")
+
+    qc_xi = 0
+    qc_n2q = 0 
+    
+    # iterate over the ordereddict to determine xi (ratio of 2 qubit gates to one qubit gates)
+    n1q = 0; n2q = 0
+    if qc_count_ops != None:
+        for key, value in qc_count_ops.items():
+            if key == "measure": continue
+            if key == "barrier": continue
+            if key.startswith("c") or key.startswith("mc"):
+                n2q += value
+            else:
+                n1q += value
+        qc_xi = n2q / (n1q + n2q)
+        qc_n2q = n2q
+    
+    return qc_depth, qc_size, qc_count_ops, qc_xi, qc_n2q
+
+#####################################################################
+from collections import OrderedDict
+from typing import Dict
+import cirq
+
+def count_ops(circuit: cirq.Circuit) -> OrderedDict[str, int]:
+    """Count each operation kind in the circuit.
+
+    Args:
+        circuit: A Cirq circuit object.
+
+    Returns:
+        OrderedDict: a breakdown of how many operations of each kind, sorted by amount.
+    """
+    count_ops: Dict[str, int] = {}
+    num_measurements = 0
+
+    def process_op(op):
+        nonlocal num_measurements
+
+        # Handle MeasurementGate separately to count number of measurements
+        if isinstance(op.gate, cirq.MeasurementGate):
+            num_measurements += len(op.qubits)
+            return  # Skip counting MeasurementGate separately
+
+        # Handle custom gates by providing a meaningful name
+        gate_name = str(op.gate)
+        
+        if '.to_gate' in gate_name:
+            qr_state = [cirq.GridQubit(i, 0) for i in range(op.gate.num_qubits)]
+            decomposed_ops = cirq.Circuit(cirq.decompose(op.gate.on(*qr_state)))
+            # print(f"\ndecomposed sub-circuit {gate_name} =====\n{decomposed_ops}")
+            for inner_op in decomposed_ops.all_operations():
+                process_op(inner_op)
+
+        count_ops[gate_name] = count_ops.get(gate_name, 0) + 1
+
+
+    # Process each operation in the circuit
+    for moment in circuit:
+        for op in moment.operations:
+            process_op(op)
+
+    # Include the number of measurements in the results
+    count_ops["Measurement"] = num_measurements
+
+    return OrderedDict(sorted(count_ops.items(), key=lambda kv: kv[1], reverse=True))
+    
+#########################################################################################
+
+# Set the state of the transpilation flags
+def set_tranpilation_flags(do_transpile_metrics = True, do_transpile_for_execute = True):
+    globals()['do_transpile_metrics'] = do_transpile_metrics
+    globals()['do_transpile_for_execute'] = do_transpile_for_execute
+
+################################################################
+
+def count_gate_operations(circuit: cirq.Circuit) -> int:
+    """Calculate the total number of gate operations in the circuit.
+
+    Args:
+        circuit: A Cirq circuit object.
+
+    Returns:
+        int: Total number of gate operations in the circuit.
+    """
+    total_gate_operations = 0
+
+    def process_op(op):
+        nonlocal total_gate_operations
+
+        if isinstance(op.gate, cirq.MeasurementGate):
+            # Count each qubit in the MeasurementGate as a separate operation
+            total_gate_operations += len(op.qubits)
+        else:
+            total_gate_operations += 1
+
+            # Handle custom gates by decomposing them if they have a .to_gate method
+            gate_name = str(op.gate)
+            if '.to_gate' in gate_name:
+                qr_state = [cirq.GridQubit(i, 0) for i in range(op.gate.num_qubits)]
+                decomposed_ops = cirq.Circuit(cirq.decompose(op.gate.on(*qr_state)))
+                for inner_op in decomposed_ops.all_operations():
+                    process_op(inner_op)
+            else:
+                # Count the operation itself
+                total_gate_operations += 1
+     
+    # Process each operation in the circuit
+    for moment in circuit:
+        for op in moment.operations:
+            process_op(op)
+
+    return total_gate_operations
+    
+###############################
+# Transpile the circuit to obtain normalized size metrics against a common basis gate set
+def transpile_for_metrics(qc):
+
+    logger.info('Entering transpile_for_metrics')
+    #print("*** Before transpile ...")
+    #print(qc)
+    st = time.time()
+
+    # Compile the circuit for CZ Target Gateset.
+    gateset = cirq.CZTargetGateset(allow_partial_czs=True)    # help(cirq.CZTargetGateset) ---> to check the gateset
+    optimized_circuit = cirq.optimize_for_target_gateset(qc, gateset=gateset)
+    cirq.testing.assert_circuits_with_terminal_measurements_are_equivalent(qc, optimized_circuit)     #To ensure optimized circuit is equivalent to original circuit in terms of final measurements
+    # print("\nCircuit compiled for CZ Target Gateset (Optimized/Transpiled ckt):", optimized_circuit, "\n", sep="\n")
+
+    # gateset = cirq.SqrtIswapTargetGateset()
+    # optimized_circuit = cirq.optimize_for_target_gateset(qc, gateset=gateset)   #sqrt_iswap_circuit
+    # # cirq.testing.assert_circuits_with_terminal_measurements_are_equivalent(qc, optimized_circuit)    # Error: optimized circuit and original circuit got differ
+    # print("\nCircuit compiled for sqrt_iswap_circuit (Optimized/Transpiled ckt):", optimized_circuit, "\n", sep="\n")
+
+    qc_tr_depth = len(cirq.Circuit(optimized_circuit.all_operations()))
+    qc_tr_size = count_gate_operations(optimized_circuit)    # total no. of gate operations (excluding barrier)
+    qc_tr_count_ops = count_ops(optimized_circuit)
+    # print(f"*** after transpile: 'qc_tr_depth' {qc_tr_depth} 'qc_tr_size' {qc_tr_size} 'qc_tr_count_ops' {qc_tr_count_ops}\n")
+    
+    # iterate over the ordereddict to determine xi (ratio of 2 qubit gates to one qubit gates)
+    n1q = 0; n2q = 0
+    if qc_tr_count_ops != None:
+        for key, value in qc_tr_count_ops.items():
+            if key == "measure": continue
+            if key == "barrier": continue
+            if key.startswith("c"): n2q += value
+            else: n1q += value
+        qc_tr_xi = n2q / (n1q + n2q) 
+        qc_tr_n2q = n2q   
+    #print(f"... qc_tr_xi = {qc_tr_xi} {n1q} {n2q}")
+    
+    logger.info(f'transpile_for_metrics - {round(time.time() - st, 5)} (ms)')
+    if verbose_time: print(f"  *** transpile_for_metrics() time = {round(time.time() - st, 5)}")
+    
+    return qc_tr_depth, qc_tr_size, qc_tr_count_ops, qc_tr_xi, qc_tr_n2q
