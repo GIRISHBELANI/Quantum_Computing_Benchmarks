e9b72149e7746f143772f7bf33e3d6fe4a0f4e97
GIRISHBELANI <girish.belani05@gmail.com>
2024-08-16 12:49:04 +0530

    For BV, improve performance of random scret integer generation

--- qc-app-oriented-benchmarks/bernstein-vazirani/qiskit/bv_benchmark.py
+++ qc-app-oriented-benchmarks/bernstein-vazirani/qiskit/bv_benchmark.py
@@ -128,7 +128,8 @@ def analyze_and_print_result (qc, result, num_qubits, secret_int, num_shots):
     
     # create the key that is expected to have all the measurements (for this circuit)
     key = format(secret_int, f"0{input_size}b")
-    
+    if verbose: print(f"... key = {key}")
+            
     # correct distribution is measuring the key 100% of the time
     correct_dist = {key: 1.0}
 
@@ -201,7 +202,9 @@ def run (min_qubits=3, max_qubits=6, skip_qubits=1, max_circuits=3, num_shots=10
         if 2**(input_size) <= max_circuits:
             s_range = list(range(num_circuits))
         else:
-            s_range = np.random.choice(2**(input_size), num_circuits, False)
+            # create selection larger than needed and remove duplicates (faster than random.choice())
+            s_range = np.random.randint(1, 2**(input_size), num_circuits + 10)
+            s_range = list(set(s_range))[0:max_circuits]
 
         # If mid circuit, then add 2 to new qubit group since the circuit only uses 2 qubits
         if method == 2:
