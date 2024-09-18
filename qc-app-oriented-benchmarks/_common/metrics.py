db5391c62b05c9c9303cb8114083923214e5f3f2
GIRISHBELANI <girish.belani05@gmail.com>
2024-09-18 16:49:00 +0530

    metrics.py corrected get_memory_usage()

--- qc-app-oriented-benchmarks/_common/metrics.py
+++ qc-app-oriented-benchmarks/_common/metrics.py
@@ -1601,7 +1601,7 @@ def modify_elapsed_times(avg_elapsed_times, avg_exec_creating_times, avg_exec_ti
 def get_memory_usage():
     usage = resource.getrusage(resource.RUSAGE_SELF)
     maximum_memory = usage.ru_maxrss/1024   # in MB 
-    return max_memory_usage
+    return maximum_memory
     
 #################################################
 
